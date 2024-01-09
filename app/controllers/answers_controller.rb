e2557219977d856a8004e0c341a4d78cdfcf8de5
Bulat <beast8357@mail.ru>
2024-01-09 15:34:18 +0300

    feat: add show action for answers_controller and specs for it

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,19 @@
 class AnswersController < ApplicationController
+  helper_method :current_question, :answer
+
+  def show
+  end
+
+  def new
+  end
+
+  private
+
+  def current_question
+    @current_question ||= Question.find(params[:question_id])
+  end
+
+  def answer
+    current_question.answers.find(params[:id])
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
 
+  describe 'GET #show' do
+    let(:answer) { create(:answer, question: question) }
+
+    before { get :show, params: { id: answer, question_id: question } }
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
 end
