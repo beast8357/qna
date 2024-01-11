ce620bdb061adfc8574e4c0c91bdfe11ae66751b
Bulat <beast8357@mail.ru>
2024-01-11 19:25:07 +0300

    refactor feature tests for questions

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -5,38 +5,30 @@ feature 'User can create a question', %q{
   As an authenticate user
   I'd like to be able to ask a question
 } do
-  given(:user) { User.create!(email: 'user@test.com', password: 'qweqwe') }
+  given(:user) { create(:user) }
 
-  scenario 'Authenticated user asks a question' do
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
+      visit questions_path
+      click_on 'Ask question'
+    end
 
-    visit questions_path
-    click_on 'Ask question'
+    scenario 'asks a question' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
+      click_on 'Ask'
 
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
-    click_on 'Ask'
+      expect(page).to have_content 'Your question has been successfully created.'
+      expect(page).to have_content 'Test question'
+      expect(page).to have_content 'text text text'
+    end
 
-    expect(page).to have_content 'Your question has been successfully created.'
-    expect(page).to have_content 'Test question'
-    expect(page).to have_content 'text text text'
-  end
-
-  scenario 'Authenticated user asks a question with errors' do
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Log in'
-
-    visit questions_path
-    click_on 'Ask question'
-
-    click_on 'Ask'
-    expect(page).to have_content "Title can't be blank"
-    expect(page).to have_content "Body can't be blank"
+    scenario 'asks a question with errors' do
+      click_on 'Ask'
+      expect(page).to have_content "Title can't be blank"
+      expect(page).to have_content "Body can't be blank"
+    end
   end
 
   scenario 'Unauthenticated user tries to ask a question' do
