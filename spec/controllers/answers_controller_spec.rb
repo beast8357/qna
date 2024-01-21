8d47e642f7a1959fefd012f137e24d8c7c99cb45
Bulat <beast8357@mail.ru>
2024-01-21 17:45:57 +0300

    fix tests for answers_controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:user) { create(:user) }
-  let(:question) { create(:question) }
+  let(:question) { create(:question, author: user) }
 
   describe 'GET #show' do
-    let(:answer) { create(:answer, question: question) }
+    let(:answer) { create(:answer, question: question, author: user) }
 
     before { get :show, params: { id: answer, question_id: question } }
 
