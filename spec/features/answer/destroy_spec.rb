287f23924a8350c86b874c4744f3ab4d01956247
Bulat <beast8357@mail.ru>
2024-01-21 17:50:48 +0300

    refactor destroy_spec for answers

--- spec/features/answer/destroy_spec.rb
+++ spec/features/answer/destroy_spec.rb
@@ -5,15 +5,14 @@ feature 'The answer can only be deleted by the author', %q{
   And an authenticated user
   I'd like to be the only one who can delete my answers
 } do
-  given(:user) { create(:user) }
-  given(:user_2) { create(:user) }
-  given(:question) { create(:question, author: user) }
-  given(:question_2) { create(:question, author: user_2) }
-  given(:answer) { create(:answer, question: question, author: user) }
-  given(:answer_2) { create(:answer, question: question, author: user_2) }
+  given(:users) { create_list(:user, 2) }
+  given(:question) { create(:question, author: users.first) }
+  given(:question_2) { create(:question, author: users.last) }
+  given(:answer) { create(:answer, question: question, author: users.first) }
+  given(:answer_2) { create(:answer, question: question, author: users.last) }
 
   describe 'User' do
-    background { sign_in(user) }
+    background { sign_in(users.first) }
 
     scenario 'tries to delete their own answer' do
       visit question_answer_path(question, answer)
