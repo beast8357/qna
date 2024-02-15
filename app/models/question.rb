5420d7057b0e12e3651abf328ee5812cd96227ca
Bulat <beast8357@mail.ru>
2024-02-15 15:51:50 +0300

    feat: add history - user can attach a file when creating a question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   has_many :answers, -> { sort_by_best }, dependent: :destroy
 
+  has_one_attached :file
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   validates :title, :body, presence: true
