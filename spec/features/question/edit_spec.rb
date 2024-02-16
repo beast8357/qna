8881e9400b2be64e4112a84929f6098eb2aeac6c
Bulat <beast8357@mail.ru>
2024-02-16 15:14:24 +0300

    feat: add history - user can attach files when editing their question

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -59,5 +59,23 @@ feature 'User can edit their question', %q{
 
       expect(page).to_not have_link 'Edit question'
     end
+
+    scenario 'can attach files when editing the question' do
+      visit question_path(question_1)
+      click_on 'Edit question'
+
+      within '.question' do
+        fill_in 'Title', with: 'edited title'
+        fill_in 'Body', with: 'edited body'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_content 'edited title'
+        expect(page).to have_content 'edited body'
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 end
