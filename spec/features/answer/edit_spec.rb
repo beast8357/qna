8e5611eee06d95e1373af6e58e01cbb7416673bb
Bulat <beast8357@mail.ru>
2024-02-16 15:21:04 +0300

    feat: add history - user can attach files when editing their answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -54,5 +54,21 @@ feature 'User can edit their answer', %q{
         expect(page).to_not have_link 'Edit'
       end
     end
+
+    scenario 'can attach file when editing the answer' do
+      sign_in(users.first)
+      visit question_path(question)
+      click_on 'Edit'
+
+      within '.answers' do
+        fill_in 'Your answer', with: 'Edited answer'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 end
