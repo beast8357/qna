d0506aa2c949b82c5f542790f8cee5f467ce06bd
Bulat <beast8357@mail.ru>
2024-02-25 17:19:39 +0300

    feat: add story - user can add multiple links when creating a question or answer

--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -8,25 +8,53 @@ feature 'User can add links to their answer', %q{
   given(:user) { create(:user) }
   given(:question) { create(:question, author: user) }
   given(:gist_url) { 'https://gist.github.com/beast8357/6bcb924bd58176ca38fdb8b43b7ea25a' }
+  given(:google_url) { 'https://www.google.com' }
 
   describe 'Authenticated user', js: true do
     background do
       sign_in(user)
       visit question_path(question)
-    end
 
-    scenario 'can add links when creating an answer' do
       within '.answer-form' do
         fill_in 'Your answer', with: 'Some answer'
 
+        click_on 'Add link'
+
         fill_in 'Link name', with: 'My gist'
         fill_in 'Url', with: gist_url
+      end
+    end
+
+    scenario 'can add a link when creating an answer' do
+      within '.answer-form' do
+        click_on 'Answer'
+      end
+
+      within '.answers' do
+        expect(page).to have_link 'My gist', href: gist_url
+      end
+
+      expect(page).to_not have_content 'Link name'
+      expect(page).to_not have_content 'Url'
+    end
+
+    scenario 'can add multiple links when creating an answer' do
+      within '.answer-form' do
+        click_on 'Add link'
+
+        within all('.nested-fields').last do
+          fill_in 'Link name', with: 'Google'
+          fill_in 'Url', with: google_url
+        end
 
         click_on 'Answer'
       end
 
       within '.answers' do
         expect(page).to have_link 'My gist', href: gist_url
+        expect(page).to have_link 'Google', href: google_url
+        expect(page).to_not have_content 'Link name'
+        expect(page).to_not have_content 'Url'
       end
     end
   end
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -7,23 +7,40 @@ feature 'User can add links to their question', %q{
 } do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/beast8357/6bcb924bd58176ca38fdb8b43b7ea25a' }
+  given(:google_url) { 'https://www.google.com' }
 
-  describe 'Authenticated user' do
+  describe 'Authenticated user', js: true do
     background do
       sign_in(user)
       visit new_question_path
-    end
 
-    scenario 'can add links when asking a question' do
       fill_in 'Title', with: 'Test question'
       fill_in 'Body', with: 'text text text'
 
+      click_on 'Add link'
+
       fill_in 'Link name', with: 'My gist'
       fill_in 'Url', with: gist_url
+    end
+
+    scenario 'can add a link when asking a question' do
+      click_on 'Ask'
+
+      expect(page).to have_link 'My gist', href: gist_url
+    end
+
+    scenario 'can add multiple links when asking a question' do
+      click_on 'Add link'
+
+      within all('.nested-fields').last do
+        fill_in 'Link name', with: 'Google'
+        fill_in 'Url', with: google_url
+      end
 
       click_on 'Ask'
 
       expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Google', href: google_url
     end
   end
 end
