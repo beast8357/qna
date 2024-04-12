5412d0134dfbb7884bc1fb4db2e9e1cc98b0c3b5
Bulat <beast8357@mail.ru>
2024-04-12 19:48:33 +0300

    feat: start implementing authentication via github

--- Gemfile
+++ Gemfile
@@ -19,6 +19,9 @@ gem 'cocoon'
 gem 'validate_url'
 gem 'pry'
 gem 'gon'
+gem 'omniauth'
+gem 'omniauth-github'
+gem 'omniauth-rails_csrf_protection'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[github]
 
   has_many :questions, foreign_key: 'author_id', dependent: :destroy
   has_many :answers,   foreign_key: 'author_id', dependent: :destroy
