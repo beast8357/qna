5ab7a7d2ac24c2b76540f28c02b4bd609963de13
Bulat <beast8357@mail.ru>
2024-05-20 13:48:14 +0300

    chore: connect pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError do |exception|
+    redirect_to root_path, alert: exception.message
+  end
 end
